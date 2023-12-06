e59468fd6331b5149ca5130fd7e76eb89115f7e3
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-06 11:14:26 +0500

    Change px to rem

--- motion/index.ts
+++ motion/index.ts
@@ -1,16 +1,16 @@
 export const menu = {
     open: {
-        width: "480px",
-        height: "650px",
-        top: "-25px",
-        right: "-25px",
+        width: "480rem",
+        height: "650rem",
+        top: "-25rem",
+        right: "-25rem",
         transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1] },
     },
     closed: {
-        width: "100px",
-        height: "40px",
-        top: "0px",
-        right: "0px",
+        width: "100rem",
+        height: "40rem",
+        top: "0rem",
+        right: "0rem",
         transition: {
             duration: 0.75,
             delay: 0.35,
