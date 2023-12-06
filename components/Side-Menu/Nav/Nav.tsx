9ad12b85d6651f8566b1a9f671e7bc356e0d3687
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-06 11:14:06 +0500

    Refacor some tailwind classes

--- components/Side-Menu/Nav/Nav.tsx
+++ components/Side-Menu/Nav/Nav.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 
 export default function Nav() {
 	return (
-		<div className="flex flex-col justify-between px-[40px] pt-[100px] pb-[50px] h-full">
-			<div className="flex gap-[10px] flex-col">
+		<div className="flex flex-col justify-between px-[40rem] pt-[100rem] pb-[50rem] h-full">
+			<div className="flex gap-[10rem] flex-col">
 				{links.map((link, i) => {
 					const { title, href } = link;
 					return (
@@ -21,7 +21,7 @@ export default function Nav() {
 								exit="exit">
 								<Link
 									href={href}
-									className="text-black text-[46px]">
+									className="text-black text-[46rem]">
 									{title}
 								</Link>
 							</motion.div>
@@ -34,7 +34,7 @@ export default function Nav() {
 					const { title, href } = link;
 					return (
 						<motion.div
-							className="w-[50%] mt-[5px]"
+							className="w-[50%] mt-[5rem] text-[16rem]"
 							variants={slideIn}
 							custom={i}
 							initial="initial"
